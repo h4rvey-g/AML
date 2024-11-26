75f600169c7e3b95606a6471f5b06f3c20236888
h4rvey-g <babaolanqiu@gmail.com>
2024-11-26 13:19:02 +0000

    Finish version 1

--- code/_targets.R
+++ code/_targets.R
@@ -2,6 +2,7 @@ library(targets)
 library(crew)
 source("code/R/101.load_data.R")
 source("code/R/102.cluster_annotate.R")
+source("code/R/103.DEG.R")
 tar_option_set(
     tidy_eval = FALSE,
     packages <- c(
@@ -20,6 +21,9 @@ list(
     tar_target(sc_int, process_sc(sc_raw)),
     tar_target(sc_cluster, cluster_data(sc_int)),
     tar_target(cell_type_path, "results/102.cluster_annotate/cell_type.tsv", format = "file"),
-    tar_target(sc_annotate, annotate_data(sc_cluster, cell_type_path))
-    # tar_target(anndata, save_annotate(sc_annotate))
+    tar_target(sc_annotate, annotate_data(sc_cluster, cell_type_path)),
+    tar_target(sc_sub_stero, sub_cluster_steroidogenic(sc_annotate)),
+    tar_target(sc_final, final_annotation(sc_annotate, sc_sub_stero)),
+    # tar_target(anndata, save_annotate(sc_final)),
+    tar_target(DEG_path, DEG_annotation(sc_final), format = "file")
 )
